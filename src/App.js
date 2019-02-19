fca7891502c18f58334cfbebd727359541c14f2f
David Rose <doppler@gmail.com>
2019-02-19 06:58:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  const pixelRatio = window.devicePixelRatio;
+  const dw = Math.floor(pixelRatio * width);
+  const dh = Math.floor(pixelRatio * height);
+  const canvas = useRef(null);
+  const style = { width, height, backgroundColor: "hsl(30, 100%, 98%)" };
+
+  const onResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+  });
+  useEffect(() => {
+    const radius = width < height ? width / 2 : height / 2;
+    const ctx = canvas.current.getContext("2d");
+    ctx.save();
+    ctx.scale(pixelRatio, pixelRatio);
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, radius, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.restore();
+  }, [width, height]);
+
+  return <canvas ref={canvas} width={dw} height={dh} style={style} />;
+};
 
 export default App;
